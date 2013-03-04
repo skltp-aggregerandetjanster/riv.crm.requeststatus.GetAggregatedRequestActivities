2b6f7fffe8ff2438b3e4f67519f99b0fcf2921dd
magnus.larsson@callistaenterprise.se <magnus.larsson@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2013-03-04 21:21:18 +0000

    Updated test registeredResidentIds to ids that exists for real test e-legs

--- src/test/java/se/skltp/aggregatingservices/riv/crm/requeststatus/getrequestactivities/RequestActivitiesIntegrationTest.java
+++ src/test/java/se/skltp/aggregatingservices/riv/crm/requeststatus/getrequestactivities/RequestActivitiesIntegrationTest.java
@@ -37,7 +37,7 @@ public class RequestActivitiesIntegrationTest extends AbstractAggregateIntegrati
 	 
 	private static final String LOGICAL_ADDRESS = "logical-address";
 	private static final String EXPECTED_ERR_TIMEOUT_MSG = "Read timed out";
-	private static final String EXPECTED_ERR_INVALID_ID_MSG = "Invalid Id: -1";
+	private static final String EXPECTED_ERR_INVALID_ID_MSG = "Invalid Id: " + TEST_RR_ID_FAULT_INVALID_ID;
 	private static final String DEFAULT_SERVICE_ADDRESS = RequestActivitiesMuleServer.getAddress("SERVICE_INBOUND_URL");
   
 	protected String getConfigResources() {
